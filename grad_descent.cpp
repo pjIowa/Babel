2dfb68eca11445f19b29c7a4c1a632fa5ef93cc3
pjIowa <pjkedilaya@gmail.com>
2016-03-13 20:54:54 -0700

    reorganized code into a class

--- grad_descent.cpp
+++ grad_descent.cpp
@@ -4,39 +4,72 @@
 #include <math.h>
 #include <utility>
 
-double computeLogitError(arma::mat weights, double bias, arma::mat input, arma::mat target);
-std::pair<arma::mat, double> updateParameters(arma::mat weights, double bias, arma::mat input, arma::mat target, double learningRate);
-std::vector<double> gradientDescent(arma::mat weights, double bias, arma::mat input, arma::mat target, double initialLearningRate, int numIterations);
 void plotData(std::vector<double> data);
-
-int main() {
-    int numIterations = 10000;
-    std::vector<double> errorList (numIterations, 0.0);
+class LogisticRegression {
+    arma::mat weights;
+    double bias;
+    arma::mat input;
+    arma::mat target;
+    double learningRate;
+    int numIterations;
     
-    std::string fileName = "diabetes.csv";
-    arma::mat csvData;
-    csvData.load(fileName, arma::csv_ascii);
-    arma::mat input = csvData.cols(0, 7);
-    arma::mat target = csvData.col(8);
-    arma::mat weights(8, 1, arma::fill::zeros);
-    double bias = 0.0;
-    double learningRate = 0.001;
-    std::cout << "Gradient Descent on " << fileName << std::endl;
-    errorList = gradientDescent(weights, bias, input, target, learningRate, numIterations);
-    plotData(errorList);
+    public:
+    LogisticRegression(arma::mat w, double b, arma::mat x, arma::mat y, double rate, int iterations) {
+        weights = w;
+        bias = b;
+        input = x;
+        target = y;
+        learningRate = rate;
+        numIterations = iterations;
+    }
     
-    fileName = "myopia.csv";
-    csvData.load(fileName, arma::csv_ascii);
-    input = csvData.cols(1, 13);
-    target = csvData.col(0);
-    weights.zeros(13, 1);
-    learningRate = 0.0001;
-    std::cout << "Gradient Descent on " << fileName << std::endl;
-    errorList = gradientDescent(weights, bias, input, target, learningRate, numIterations);
-    plotData(errorList);
+    double computeLogitError() {
+        double totalError = 0.0;
+        for(int i=0; i<input.n_rows; i++) {
+            double weightedSum = bias;
+            for(int j=0; j<input.n_cols; j++) {
+                 weightedSum += weights(j)*input(i,j);
+            }
+            double prediction = 1.0/(1.0 + exp(-weightedSum));
+            totalError += target(i) - prediction;
+        }
+        return totalError / input.n_rows;
+    }
     
-    return 0;
-}
+    void updateParameters() {
+        double biasGradient = 0.0;
+        arma::mat weightGradients(weights.n_rows, 1, arma::fill::zeros);
+        double N = input.n_rows;
+        for(int i=0; i<N; i++) {
+            double weightedSum = bias;
+            for(int j=0; j<input.n_cols; j++) {
+                weightedSum += weights(j) * input(i,j);
+            }
+            double prediction = 1.0 / (1.0 + exp(-weightedSum));
+            
+            biasGradient += -2.0/N * (target[i] - prediction);
+            weightGradients += -2.0/N * input[i] * (target[i] - prediction);
+        }
+        weights = weights - learningRate * weightGradients;
+        bias = bias - learningRate * biasGradient;
+    }
+    
+    std::vector<double> gradientDescent() {
+        double logitError = computeLogitError();
+        std::vector<double> errorList (numIterations, 0.0);
+        
+        for(int i=0; i<numIterations; i++) {
+            errorList[i] = logitError;
+            if (i%1000==0) {
+                std::cout << "Step " << i << " " << logitError << std::endl;
+            }
+            updateParameters();
+            logitError = computeLogitError();
+        }
+        
+        return errorList;
+    }
+};
 
 void plotData(std::vector<double> data) {
     FILE *pipe = popen("gnuplot -persist" , "w");
@@ -59,56 +92,34 @@ void plotData(std::vector<double> data) {
     }
 }
 
-std::vector<double> gradientDescent(arma::mat weights, double bias, arma::mat input, arma::mat target, double initialLearningRate, int numIterations) {
-    double logitError = computeLogitError(weights, bias, input, target);
-    double learningRate = initialLearningRate;
-    
+int main() {
+    int numIterations = 10000;
     std::vector<double> errorList (numIterations, 0.0);
+    std::string fileName = "diabetes.csv";
+    arma::mat csvData;
+    csvData.load(fileName, arma::csv_ascii);
+    arma::mat input = csvData.cols(0, 7);
+    arma::mat target = csvData.col(8);
+    arma::mat weights(8, 1, arma::fill::zeros);
+    double bias = 0.0;
+    double learningRate = 0.001;
     
-    for(int i=0; i<numIterations; i++) {
-        errorList[i] = logitError;
-        if (i%1000==0) {
-            std::cout << "Step " << i << " " << logitError << std::endl;
-        }
-        std::pair<arma::mat, double> updatedSet = updateParameters(weights, bias, input, target, learningRate);
-        weights = updatedSet.first;
-        bias = updatedSet.second;
-        logitError = computeLogitError(weights, bias, input, target);
-    }
+    std::cout << "Gradient Descent on " << fileName << std::endl;
+    LogisticRegression model(weights, bias, input, target, learningRate, numIterations);
+    errorList = model.gradientDescent();
+    plotData(errorList);
     
-    return errorList;
-}
-
-double computeLogitError(arma::mat weights, double bias, arma::mat input, arma::mat target) {
-    double totalError = 0.0;
-    for(int i=0; i<input.n_rows; i++) {
-        double weightedSum = bias;
-        for(int j=0; j<input.n_cols; j++) {
-             weightedSum += weights(j)*input(i,j);
-        }
-        double prediction = 1.0/(1.0 + exp(-weightedSum));
-        totalError += target(i) - prediction;
-    }
-    return totalError / input.n_rows;
-}
-
-std::pair<arma::mat, double> updateParameters(arma::mat weights, double bias, arma::mat input, arma::mat target, double learningRate) {
-    double biasGradient = 0.0;
-    arma::mat weightGradients(weights.n_rows, 1, arma::fill::zeros);
-    double N = input.n_rows;
-    for(int i=0; i<N; i++) {
-        double weightedSum = bias;
-        for(int j=0; j<input.n_cols; j++) {
-            weightedSum += weights(j) * input(i,j);
-        }
-        double prediction = 1.0 / (1.0 + exp(-weightedSum));
-        
-        biasGradient += -2.0/N * (target[i] - prediction);
-        weightGradients += -2.0/N * input[i] * (target[i] - prediction);
-    }
+    fileName = "myopia.csv";
+    csvData.load(fileName, arma::csv_ascii);
+    input = csvData.cols(1, 13);
+    target = csvData.col(0);
+    weights.zeros(13, 1);
+    learningRate = 0.0001;
     
-    arma::mat retWeights = weights - learningRate * weightGradients;
-    double retBias = bias - learningRate * biasGradient;
-    std::pair<arma::mat, double> retPair(retWeights, retBias);
-    return retPair;
+    std::cout << "Gradient Descent on " << fileName << std::endl;
+    model = LogisticRegression(weights, bias, input, target, learningRate, numIterations);
+    errorList = model.gradientDescent();
+    plotData(errorList);
+    
+    return 0;
 }
