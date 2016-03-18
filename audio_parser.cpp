cf139f2165778c52097d7868077141ba31c13573
pjIowa <pjkedilaya@gmail.com>
2016-03-17 22:25:52 -0700

    performed fft on signal chunks after hanning function

--- audio_parser.cpp
+++ audio_parser.cpp
@@ -6,13 +6,18 @@
 #include <sndfile.hh>
 #include <cmath>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 #include <fftw3.h>
 
-std::pair<std::vector<double>, SF_INFO>  readWaveFile(std::string fileName);
+#define PI 3.14159265
 
-std::pair<std::vector<double>, SF_INFO> readWaveFile(std::string fileName) {
+std::pair<arma::mat, SF_INFO>  readWaveFile(std::string fileName);
+arma::mat parseFrequencyStrengths(arma::mat rawSound);
+
+std::pair<arma::mat, SF_INFO> readWaveFile(std::string fileName) {
     SF_INFO soundInfo;
-    std::vector<double> buffer = std::vector<double>();
+    arma::mat buffer;
     SNDFILE *sndFile = sf_open(fileName.c_str(), SFM_READ, &soundInfo);
     
     if (sndFile == NULL) {
@@ -23,50 +28,92 @@ std::pair<std::vector<double>, SF_INFO> readWaveFile(std::string fileName) {
         sf_close(sndFile);
     }
     else {
-        int numberOfFrames = soundInfo.frames;
-        int numberOfChannels = soundInfo.channels;
-        double *tempBuffer = new double[numberOfFrames*numberOfChannels];
-        if (tempBuffer == NULL) {
-            fprintf(stderr, "Could not allocate memory for data\n");
+        long numFrames = soundInfo.frames;
+        long numChannels = soundInfo.channels;
+        long numElems = numFrames*numChannels;
+        double* tempBuffer = new double[numElems];
+        buffer.zeros(numFrames, numChannels);
+        long numParsedFrames = sf_readf_double(sndFile, tempBuffer, soundInfo.frames);
+        if (numParsedFrames != numFrames) {
+            fprintf(stderr, "Did not read enough frames for source\n");
         }
-        else {
-            long numFrames = sf_readf_double(sndFile, tempBuffer, soundInfo.frames);
-            if (numFrames != soundInfo.frames) {
-                fprintf(stderr, "Did not read enough frames for source\n");
-            }
-            //TODO: copy data from tempBuffer into buffer
+        for(long i=0; i<numElems; i++) {
+            long row = i / numChannels;
+            long column = i % numChannels;
+            buffer(row,column) = tempBuffer[i];
         }
-        
         free(tempBuffer);
         sf_close(sndFile);
     }
     return std::make_pair(buffer, soundInfo);
 }
 
+arma::mat parseFrequencyStrengths(arma::mat rawSound) {
+    arma::mat retMatrix;
+    retMatrix.zeros(size(rawSound));
+    
+    //break into equal chunks of sample points, n should be power of 2
+    //start count at 128, higher count is higher frequency resolution 
+    //Andrew Ng uses 20ms in paper, so try up to 1024
+    long n = 128;
+    long rows = rawSound.n_rows;
+    long cols = rawSound.n_cols;
+    long numIntervals = rows/n;
+    
+    for(long i=0; i<=numIntervals; i++) {
+        
+        //try 50% overlap, retain information lost from window function
+        long start = i*n;
+        long end = (i+1)*n-1;
+        if (i==numIntervals) {
+            end = rows-1;
+        }
+        long fftLength = end-start+1;
+        
+        arma::uvec fftRange = arma::linspace<arma::uvec>(start, end, fftLength);
+        for(double j=0; j<cols; j++) {
+            arma::uvec colVec;
+            colVec << j;
+            arma::cx_mat chunk = arma::conv_to<arma::cx_mat>::from(rawSound.submat(fftRange, colVec));
+            
+            //apply hann window function, reduce spectral leakage
+            for (long i=0; i<fftLength; i++) {
+                double multiplier = 0.5 * (1 - cos(2*PI*i/(n-1)));
+                chunk(i,0) = multiplier * chunk(i,0);
+            }
+            
+            //apply fft
+            //try fftw library for speed
+            fftw_complex* in = reinterpret_cast<fftw_complex*> (chunk.colptr(0));
+            fftw_plan plan = fftw_plan_dft_1d(fftLength, in, in, FFTW_FORWARD, FFTW_MEASURE);
+            fftw_execute(plan);
+            
+            //keep all frequencies below nyquist frequency, sampleFrequency/2
+            //if fftLength is even, keep the nyquist frequency too
+            
+            //scale by n, remove effect on magnitude from length of signal
+            //apply absolute value, combines R & I components
+            
+            //apply 20*log, converts magnitude to dB scale
+            //each chunk now provides accurate frequency strength over time
+        }
+    }
+    
+    return retMatrix;
+}
+
 int main (void) {
     
     //Create Spectrogram
     //get raw signal and sound information
-    
-    std::pair<std::vector<double>, SF_INFO> waveData = readWaveFile("440_sine.wav");
-    std::vector<double> buffer = waveData.first;
+    std::pair<arma::mat, SF_INFO> waveData = readWaveFile("440_sine.wav");
+    arma::mat buffer = waveData.first;
     SF_INFO soundInfo = waveData.second;
     
-    //apply hann window function, reduce spectral leakage
-    //break into equal chunks of sample points, count should be power of 2
-    //use 50% overlap, retain information lost from window function
-    //start count at 128, higher count is higher frequency resolution 
-    //Andrew Ng uses 20ms in paper, so also try 1024
-    //for each chunk and channel
-        //set n to number of sample points
-        //determine correct window function and apply
-        //apply fft, use fftw library for speed
-        //keep all frequencies below nyquist frequency, sampleFrequency/2
-        //if n is even, keep the nyquist frequency too
-        //scale by n, remove effect on magnitude from length of signal
-        //apply absolute value, combines R & I components
-        //apply 20*log, converts magnitude to dB scale
-        //each chunk now provides accurate frequency strength over time
+    //get frequency strengths per time period
+    arma::mat parsedStrengths = parseFrequencyStrengths(buffer);
+    
+    //map in gnuplot
     
     return 0;
 }
