c798e58650bf4c01d9107e69b40a4cbfb409046c
pjIowa <pjkedilaya@gmail.com>
2016-03-24 22:55:53 -0700

    calculated first part of training

--- neural_net.cpp
+++ neural_net.cpp
@@ -9,28 +9,33 @@ class NeuralNetwork {
     arma::mat target;
     arma::mat weights;
     int numIterations;
-    
+
     arma::mat sigmoid(arma::mat x) {
         return 1.0 / (1.0 + arma::exp(-1.0*x));
     }
-    
+
     arma::mat sigmoid_derivative(arma::mat x) {
         return x * (1-x);
     }
-    
+
     void randomInitWeights() {
-        //weight dimensions: input.n_rows x 1
+        weights.randu(1, input.n_cols);
+        weights.elem( find(weights > 1.0) ).ones();
+        weights.elem( find(weights < -1.0) ).fill(-1.0);
     }
-    
+
     public:
     NeuralNetwork(arma::mat i, arma::mat t) {
         input = i;
         target = t;
         randomInitWeights();
     }
-    
+
     void train(int numIt) {
         numIterations = numIt;
+        for(int i=0; i<numIterations; i++) {
+            arma::mat output = input.each_row() % weights;
+        }
     }
 };
 
@@ -41,7 +46,7 @@ int main() {
     //1x4
     arma::mat target = {{0, 1, 1, 0}};
     int numIterations = 1000;
-    
+
     NeuralNetwork model(input, target);
     model.train(numIterations);
     return 0;
