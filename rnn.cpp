da48c5a50ea001c73989c99a2c395e29f5903bf3
pjIowa <pjkedilaya@gmail.com>
2016-04-11 22:11:10 -0700

    completed weight updates using rprop and trained model

--- rnn.cpp
+++ rnn.cpp
@@ -7,6 +7,10 @@ int main (void) {
     arma::mat target =  sum(input, 1);
     
     RecurrentNeuralNetwork model(input, target);
-    model.train(5000);
+    std::clock_t startTime = std::clock();
+    model.train(500);
+    std::cout << "Training Time: " << (std::clock() - startTime) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+    
+    //TODO: add test data
     return 0;
 }
--- rnn.h
+++ rnn.h
@@ -3,10 +3,14 @@
 #include <armadillo>
 
 class RecurrentNeuralNetwork {
-    //TODO: separate weights into input and context weights
-    arma::mat weights;
-    arma::mat weightDeltas;
-    arma::mat previousWeightSigns;
+    arma::mat inputWeights;
+    arma::mat contextWeights;
+    
+    arma::mat inputWeightDeltas;
+    arma::mat contextWeightDeltas;
+    
+    arma::mat previousInputWeightSigns;
+    arma::mat previousContextWeightSigns;
     
     double etaP = 1.2;
     double etaN = 0.5;
@@ -25,18 +29,9 @@ class RecurrentNeuralNetwork {
     
     void randomInitWeights() {
         arma::arma_rng::set_seed(1);
-        weights.randn(1, 2);
-    }
-    
-    //return context for each time step
-    arma::mat forwardStep(arma::mat inputWeights, arma::mat contextWeights) {
-        arma::mat context = arma::zeros<arma::mat>(input.n_rows, input.n_cols + 1);
-        
-        for(int i=0; i<input.n_cols; i++) {
-            context.col(i+1) = updateContext(input.col(i), context.col(i), inputWeights, contextWeights);
-        }
-        
-        return context;
+        //TODO: make sizes dynamic
+        inputWeights.randn(1, 1);
+        contextWeights.randn(1, 1);
     }
     
     //take input for time step, previous context, input weights, and context weights
@@ -53,10 +48,10 @@ class RecurrentNeuralNetwork {
     
     //take context for all time steps, output gradient, and context weight
     //return gradient update for input and context weights
-    std::vector<arma::mat> backwardGradient(arma::mat context, arma::mat gradientOutput, arma::mat contextWeights) {
+    std::vector<arma::mat> backwardGradient(arma::mat context, arma::mat gradientOutput) {
         arma::mat gradientOverTime = arma::zeros<arma::mat>(input.n_rows, input.n_cols + 1);
         gradientOverTime.col(gradientOverTime.n_cols-1) = gradientOutput;
-        //TODO: generalize
+        //TODO: make sizes dynamic
         arma::mat inputGradient = { 0 };
         arma::mat contextGradient = { 0 };
         
@@ -69,24 +64,32 @@ class RecurrentNeuralNetwork {
     }
     
     void resilientPropagationUpdate() {
-        arma::mat inputWeights = weights.col(0);
-        arma::mat contextWeights = weights.col(1);
-        
-        arma::mat context = forwardStep(inputWeights, contextWeights);
+        //Forward calculations
+        arma::mat context = forwardStep();
         arma::mat gradientOutput = outputGradient(context.col(context.n_cols-1));
-        std::vector<arma::mat> weightGradients = backwardGradient(context, gradientOutput, contextWeights);
-        //get sign of weight gradients
-        for(int i=0; i<weights.n_cols; i++) {
-            //if new weight sign is same as previous weight sign
-                //multiply delta by etaP
-            //else
-                //multiply delta by etaN
+        
+        //Backward calculations
+        std::vector<arma::mat> weightGradients = backwardGradient(context, gradientOutput);
+        
+        //Get the sign (-1, 0, 1) of the weight updates
+        std::vector<arma::mat> weightGradientSigns;
+        for(int i=0; i<weightGradients.size(); i++) {
+            weightGradientSigns.push_back(sign(weightGradients[i]));
         }
-        //store new signs in previous sign
-        for(int i=0; i<weights.n_cols; i++) {
-            arma::mat weight = weights.col(i);
-            //subtract sign*delta from weight
-        }
+        arma::mat inputGradientSigns = weightGradientSigns[0];
+        arma::mat contextGradientSigns = weightGradientSigns[1];
+        
+        //Update magnitude of weight changes
+        inputWeightDeltas.elem( find(inputGradientSigns == previousInputWeightSigns) )*etaP;
+        inputWeightDeltas.elem( find(inputGradientSigns != previousInputWeightSigns) )*etaN;
+        contextWeightDeltas.elem( find(contextGradientSigns == previousContextWeightSigns) )*etaP;
+        contextWeightDeltas.elem( find(contextGradientSigns != previousContextWeightSigns) )*etaN;
+        previousInputWeightSigns = inputGradientSigns;
+        previousContextWeightSigns = contextGradientSigns;
+        
+        //Update weights
+        inputWeights -= inputGradientSigns%inputWeightDeltas;
+        contextWeights -= contextGradientSigns%contextWeightDeltas;
     }
     
     public:
@@ -97,20 +100,27 @@ class RecurrentNeuralNetwork {
         input = i;
         target = t;
         randomInitWeights();
-        //TODO: generalize
-        weightDeltas = {0.001, 0.001};
-        previousWeightSigns = {0, 0};
+        
+        //TODO: make sizes dynamic
+        inputWeightDeltas = {0.001};
+        contextWeightDeltas = {0.001};
+        previousInputWeightSigns = {0};
+        previousContextWeightSigns = {0};
     } 
     
+        //return context for each time step
+    arma::mat forwardStep() {
+        arma::mat context = arma::zeros<arma::mat>(input.n_rows, input.n_cols + 1);
+        for(int i=0; i<input.n_cols; i++) {
+            context.col(i+1) = updateContext(input.col(i), context.col(i), inputWeights, contextWeights);
+        }
+        return context;
+    }
+    
     void train(int numIt) {
-        resilientPropagationUpdate();
-//        for(int i=0; i<numIt; i++) {
-//            
-//        }
-        
-//        
-//        for(int t=0; t<input.n_rows; t++) {
-//        }
+        for(int i=0; i<numIt; i++) {
+            resilientPropagationUpdate();
+        }
     }
 };
 #endif
