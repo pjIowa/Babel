a50db5daf216fecf67e620c5c2afa94a50c8abd4
pjIowa <pjkedilaya@gmail.com>
2016-04-14 22:42:44 -0700

    made different input and target sequences for training, working on generalized rnn

--- rnnGeneral.cpp
+++ rnnGeneral.cpp
@@ -1,13 +1,27 @@
 #include "rnnGeneral.h"
 
+double isXOR(double a, double b);
+
+double isXOR(double a, double b) {
+    double notA = a==0;
+    double notB = b==0;
+    return (a==notB) | (notA==b);
+}
+
 int main (void) {
     int sequenceLength = 10;
     int numberOfSequences = 20;
+    
     arma::mat input = round(arma::randu<arma::mat>(numberOfSequences, sequenceLength));
-    arma::mat target =  sum(input, 1);
+    arma::mat target = arma::zeros<arma::mat>(numberOfSequences, sequenceLength-1);
+    for(int i=0; i<input.n_rows; i++) {
+        for(int j=0; j<input.n_cols-1; j++) { 
+            target(i, j) = isXOR( input(i, j), input(i, j+1) ); 
+        }
+    }
     
     std::cout << std::endl;
-    std::cout << "Recurrent Neural Network trained to sum an input sequence" << std::endl;
+    std::cout << "Non-Linear Recurrent Neural Network trained to XOR a sequence" << std::endl;
     RecurrentNeuralNetwork model(input, target);
     std::clock_t startTime = std::clock();
     std::vector<double> costVector = model.train(1700);
@@ -17,14 +31,6 @@ int main (void) {
     for(int i=0; i<costVector.size(); i+=200) {
         std::cout << "Step " << i << "  \t" << costVector[i] << std::endl;
     }
-    std::cout << std::endl;
-    
-    arma::mat testInput = {{0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1}};
-    model.input = testInput;
-    arma::mat testOutput = model.forwardStep();
-    std::cout << std::endl;
-    std::cout << "Expectation on Test Sequence: " << accu(testInput) << std::endl;
-    std::cout << "Prediction:                   " << as_scalar(testOutput.col(testOutput.n_cols-1)) << std::endl;
     std::cout << std::endl;
     return 0;
 }
--- rnnGeneral.h
+++ rnnGeneral.h
@@ -4,6 +4,11 @@
 
 class RecurrentNeuralNetwork {
     
+    double learningRate = 0.1;
+    int numContextNodes = 2;
+    arma::mat inputWeights, contextWeights, outputWeights;
+    
+    //Neural network boilerplate
     arma::mat sigmoid_derivative(arma::mat x) {
         return x % (1-x);
     }
@@ -18,13 +23,92 @@ class RecurrentNeuralNetwork {
     
     void randomInitWeights() {
         arma::arma_rng::set_seed(1);
+        inputWeights.randn(1, numContextNodes);
+        contextWeights.randn(numContextNodes, numContextNodes);
+        outputWeights.randn(numContextNodes, target.n_cols);
     }
     
-    double propagationUpdate() {
-        double error = 0.0;
+    //Forward step methods
+    arma::mat updateContext(arma::mat inputRow, arma::mat contextRow) {
+        return inputRow*inputWeights + contextRow*contextWeights;
+    }
+    
+    std::vector<arma::mat> contextOverTime() {
+        std::vector<arma::mat> cOT;
+        cOT.push_back(arma::zeros<arma::mat>(input.n_rows, numContextNodes));
+        for(int i=0; i<input.n_cols; i++) {
+            cOT.push_back(updateContext(input.col(i), cOT[i]));
+        }
+        return cOT;
+    }
+    
+    double cost(arma::mat prediction) {
+        return accu(square(target - prediction))/target.n_rows;
+    }
+    
+    //Backward step methods
+    arma::mat outputGradient(arma::mat prediction) {
+        return 2.0*(prediction-target)/target.n_rows;
+    }
+    
+    std::vector<arma::mat> backwardGradients(std::vector<arma::mat> contextOverTime, arma::mat gradientOutput) {
+        std::vector<arma::mat> cGOT, iGOT;
+        arma::mat lastContextGradient = gradientOutput*outputWeights.t();
+        arma::mat lastInputGradient = lastContextGradient*inputWeights.t();
+//        std::cout << size(gradientOutput) << std::endl;
+//        std::cout << size(outputWeights) << std::endl;
+//        std::cout << size(input.col(0)) << std::endl;
+//        std::cout << size(inputWeights) << std::endl;
+//        std::cout << size(contextWeights) << std::endl;
+//        std::cout << size(lastContextGradient) << std::endl;
+//        std::cout << size(lastInputGradient*input.col(input.n_cols-1)) << std::endl;
+//        std::cout << size(sum(lastContextGradient*input.col(input.n_cols-1), 1)) << std::endl;
         
+        cGOT.push_back(lastContextGradient);
+        iGOT.push_back(lastInputGradient);
+        
+        arma::mat inputGradientAccumulator;
+        inputGradientAccumulator.zeros( size(inputWeights) );
+        arma::mat contextGradientAccumulator;
+        contextGradientAccumulator.zeros( size(contextWeights) );
+        
+        for(int i=0; i<input.n_cols; i++) {
+//            std::cout << "step 1" << std::endl;
+            inputGradientAccumulator += sum(iGOT[i]%input.col(input.n_cols-i-1));
+            std::cout << "step 2" << std::endl;
+            contextGradientAccumulator += sum(cGOT[i]*contextOverTime[input.n_cols-i-1]);
+            std::cout << "step 3" << std::endl;
+            cGOT.push_back(cGOT[i]*contextWeights);
+            iGOT.push_back(iGOT[i]*inputWeights);
+            std::cout << "step 4" << std::endl;
+        }
+        
+        std::vector<arma::mat> retVec;
+        retVec.push_back(inputGradientAccumulator);
+        retVec.push_back(contextGradientAccumulator);
+        
+        return retVec;
+    }
+    
+    //Model update step
+    double updateModel() {
+        double error = 0.0;
+//        std::cout << "step 1" << std::endl;
+        std::vector<arma::mat> cOT = contextOverTime();
+//        std::cout << "step 2" << std::endl;
+        arma::mat prediction = cOT.back()*outputWeights;
+        arma::mat gradientOutput = outputGradient(prediction);
+//        std::cout << "step 3" << std::endl;
+        std::vector<arma::mat> weightGradients = backwardGradients(cOT, gradientOutput);
+        std::cout << "step 4" << std::endl;
         return error;
     }
+    
+    //placeholder
+//    double updateModel() {
+//        double error = 0.0;
+//        return error;
+//    }
     
     public:
     arma::mat input;
@@ -34,17 +118,12 @@ class RecurrentNeuralNetwork {
         input = i;
         target = t;
         randomInitWeights();
-    } 
-    
-    std::vector<arma::mat> forwardStep() {
-        std::vector<arma::mat> ctx;
-        return ctx;
     }
     
     std::vector<double> train(int numIt) {
         std::vector<double> costVector;
         for(int i=0; i<numIt; i++) {
-            double cost = propagationUpdate();
+            double cost = updateModel();
             costVector.push_back(cost);
         }
         return costVector;
