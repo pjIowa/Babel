59407631850c422a4993b1305bbf09b100592897
pjIowa <pjkedilaya@gmail.com>
2016-06-25 16:24:29 -0700

    updated gitignore, made picture classifier into class

--- pictureClassifier.py
+++ pictureClassifier.py
@@ -1,24 +1,24 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import struct
 import os
 
-def openData():
-    with open('train-labels-idx1-ubyte', 'rb') as flbl:
-        magic, num = struct.unpack(">II", flbl.read(8))
-        labels = np.fromfile(flbl, dtype=np.int8)
+class PictureClassifier:
     
-    with open('train-images-idx3-ubyte', 'rb') as fimg:
-        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
-        images = np.fromfile(fimg, dtype=np.uint8).reshape(len(labels), rows, cols)
+    def __init__(self):
+        self.loadData()
+        
+    def loadData(self):
+        with open('train-labels-idx1-ubyte', 'rb') as flbl:
+            magic, num = struct.unpack(">II", flbl.read(8))
+            self.labels = np.fromfile(flbl, dtype=np.int8)
     
-    return labels, images
+        with open('train-images-idx3-ubyte', 'rb') as fimg:
+            magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            self.images = np.fromfile(fimg, dtype=np.uint8).reshape(len(self.labels), rows, cols)
+    
+    def getDim(self):
+        print self.labels.shape
+        
 
-
-labels, images = openData()
-
-#statisitics and display example image
-print labels.shape
-print images.shape
-plt.imshow(images[0], cmap='Greys_r')
-plt.show()
+c = PictureClassifier()
+c.getDim()
