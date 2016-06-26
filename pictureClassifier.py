e764d91d8235ebbb41c0ac9c916cbca2fbc96a56
pjIowa <pjkedilaya@gmail.com>
2016-06-25 22:37:18 -0700

    cleaned up code and added bounds checking

--- pictureClassifier.py
+++ pictureClassifier.py
@@ -6,6 +6,7 @@ class PictureClassifier:
     
     def __init__(self):
         self.loadData()
+        self.f = lambda x: 1.0/(1.0 + np.exp(np.clip(-x, -709, 709)))
         
     def loadData(self):
         with open('train-labels-idx1-ubyte', 'rb') as flbl:
@@ -28,18 +29,9 @@ class PictureClassifier:
         np.random.seed(0)
         self.weights = np.random.randn(self.imRows*self.imCols, self.numUniqueLabels)
         self.biases = np.random.randn(self.numUniqueLabels)
-        self.weights[self.weights < 0] = 0
-        self.weights[self.weights > 1] = 1
-        self.biases[self.biases < 0] = 0
-        self.biases[self.biases > 1] = 1
-        
-#        print np.amax(self.weights)
-#        print np.amax(self.biases)
-#        print self.trainImages.shape, self.testImages.shape, self.weights.shape, self.biases.shape
-#        print self.trainLabels.shape, self.testLabels.shape
     
     def predict(self, x):
-        predictions = 1.0/(1.0+np.exp(np.dot(x, -self.weights)))+self.biases
+        predictions = self.f(np.dot(x, self.weights) + self.biases)
         predictions[predictions < 10**-10] = 10**-10
         predictions[predictions >= 1] = 1-10**-10
         return predictions
@@ -53,13 +45,9 @@ class PictureClassifier:
     def updateWeights(self):
         predictError = self.predict(self.trainImages)-self.trainLabels
         weightGrads = np.dot((predictError).T, self.trainImages).T
-        learnRate = 0.000001
+        learnRate = 0.0000001
         self.weights -= learnRate/self.numExamples * np.sum(weightGrads)
         self.biases -= learnRate/self.numExamples * np.sum(predictError)
-        self.weights[self.weights < 0] = 0
-        self.weights[self.weights > 1] = 1
-        self.biases[self.biases < 0] = 0
-        self.biases[self.biases > 1] = 1
         
 
 c = PictureClassifier()
